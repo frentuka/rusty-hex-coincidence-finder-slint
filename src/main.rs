be6a4efcc210349e53068af1aa22c987fd6ca7cf
frentuka <frentuka@gmail.com>
2025-03-31 20:49:55 -0300

    Interface and method-of-use changes:
    Now, when checking for a coincidence, all available coincidences will be provided in order for the user to choose which one to modify with the desired value.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod analysis;
 mod inout;
 mod ui_management;
 
+use std::collections::HashMap;
 use std::error::Error;
 use std::ops::Deref;
 use std::rc::Rc;
@@ -15,36 +16,42 @@ slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
-
+    
     /*
         GameSaves UI Array
      */
-    let gamesaves =  Rc::from(VecModel::default());
-    let gamesaves_filepaths = Rc::from(VecModel::default());
-    let values = Rc::from(VecModel::default());
-    let found_coincidences: i32 = -1;
+    // (filename, path)
+    let gamesave_list: Rc<VecModel<(SharedString, SharedString)>> = Rc::from(VecModel::default());
+    let gamesave_values: Rc<VecModel<i32>> = Rc::from(VecModel::default());
 
-    ui.set_gamesaves(ModelRc::from(gamesaves.clone()));
-    ui.set_gamesaves_paths(ModelRc::from(gamesaves_filepaths.clone()));
-    ui.set_values(ModelRc::from(values.clone()));
-    ui.set_found_coincidences(found_coincidences.clone());
+    ui.set_gamesave_list(ModelRc::from(gamesave_list.clone()));
+    ui.set_gamesave_values(ModelRc::from(gamesave_values.clone()));
+
+    /*
+        Coincidence selection stuff
+     */
+    let found_coincidences_list: Rc<VecModel<(i32, i32)>> = Rc::from(VecModel::default());
+
+    ui.set_checked_for_coincidences(false);
+    ui.set_found_coincidences_list(ModelRc::from(found_coincidences_list.clone()));
 
     /*
         Add Savegame button
      */
-    let gamesaves_add = gamesaves.clone();
-    let gamesaves_filepaths_add = gamesaves_filepaths.clone();
-    let values_add = values.clone();
+    let gamesave_list_add = gamesave_list.clone();
+    let gamesave_values_add = gamesave_values.clone();
     ui.on_request_add_savegame({
         move || {
-
             // Open file explorer
             let file = ui_management::add_gamesave();
             if file.is_some() {
+                // 0: path. 1: filename.
                 let file_unwrap = file.unwrap();
-                gamesaves_filepaths_add.push(SharedString::from(file_unwrap.0));
-                gamesaves_add.push(SharedString::from(file_unwrap.1));
-                values_add.push(0);
+                let filename = SharedString::from(file_unwrap.0);
+                let filepath = SharedString::from(file_unwrap.1);
+
+                gamesave_list_add.push((filename, filepath));
+                gamesave_values_add.push(0);
             };
         }
     });
@@ -52,49 +59,48 @@ fn main() -> Result<(), Box<dyn Error>> {
     /*
         Remove Savegame button
      */
-    let gamesaves_remove = gamesaves.clone();
-    let gamesaves_filepath_remove = gamesaves_filepaths.clone();
-    let values_remove = values.clone();
+    let gamesave_list_remove = gamesave_list.clone();
+    let gamesave_values_remove = gamesave_values.clone();
     ui.on_request_remove_savegame(move |name: SharedString| {
         // find index of name in gamesaves_remove
-        let index = gamesaves_remove.iter().position(|x| x == &name).unwrap();
-        gamesaves_remove.remove(index);
-        gamesaves_filepath_remove.remove(index);
-        values_remove.remove(index);
+        let index = gamesave_list_remove.iter().position(|x| x.0 == &name).unwrap();
+        gamesave_list_remove.remove(index);
+        gamesave_values_remove.remove(index);
     });
 
     /*
         let gamesaves_update
      */
-    let gamesaves_updatevalue = gamesaves.clone();
-    let values_updatevalue = values.clone();
+    let gamesave_list_updatevalue = gamesave_list.clone();
+    let values_updatevalue = gamesave_values.clone();
     ui.on_update_savegame_value(move |name: SharedString, val: SharedString| {
         // check for a valid i32
-        let val = match val.parse::<i32>() {
-            Ok(val) => val,
-            _ => 0
-        };
+        let val = val.parse::<i32>();
 
-        // find index
-        let index = gamesaves_updatevalue.iter().position(|x| x == &name).unwrap();
-        // modify value
-        if index == values_updatevalue.iter().len() { values_updatevalue.push(val) }
-        else { values_updatevalue.set_row_data(index, val) }
+        if val.is_ok() {
+            let val = val.unwrap();
+            // find index
+            let index = gamesave_list_updatevalue.iter().position(|x| x.0 == &name).unwrap();
+            // modify value
+            if index == values_updatevalue.iter().len() { values_updatevalue.push(val) }
+            else { values_updatevalue.set_row_data(index, val) }
+        }
     });
 
 
     /*
         refresh
      */
-    let values_refresh = values.clone();
-    let gamesaves_filepaths_refresh = gamesaves_filepaths.clone();
+    let values_refresh = gamesave_values.clone();
+    let gamesave_list_refresh = gamesave_list.clone();
+    let found_coincidences_list_refresh = found_coincidences_list.clone();
     let ui_handle_refresh = ui.as_weak();
     ui.on_request_refresh(move || {
         let ui = ui_handle_refresh.unwrap();
 
         let mut archivos: Vec<Vec<u8>> = Vec::new();
-        for filepath in gamesaves_filepaths_refresh.iter() {
-            archivos.push(inout::read_binary_file(filepath.as_str()).unwrap());
+        for filepath in gamesave_list_refresh.iter() {
+            archivos.push(inout::read_binary_file(filepath.1.as_str()).unwrap());
         }
 
         let mut values: Vec<u32> = Vec::new();
@@ -108,39 +114,41 @@ fn main() -> Result<(), Box<dyn Error>> {
         let anal = analysis::find_consistent_positions_u32(&archivos, &values);
         let lineas: Vec<u32> = anal.iter().filter_map(|t| Some((((t+1) as f32)/16.0).round() as u32)).collect();
 
-        ui.set_found_coincidences(anal.len() as i32);
-        if anal.len() == 1 {
-            ui.set_unique_coincidence_index(*anal.get(0).unwrap() as i32);
-
-            // get first file in gamesaves_filepaths_refresh
-            let first_file: SharedString = gamesaves_filepaths_refresh.iter().next().unwrap().clone();
-            ui.set_gamesave_to_clone_filepath(first_file);
+        found_coincidences_list_refresh.clear();
+        for i in 0..anal.len() {
+            found_coincidences_list_refresh.push((
+                anal[i] as i32,
+                lineas[i] as i32
+            ));
         }
+        
+        println!("Found Coincidences List: {:?}", found_coincidences_list_refresh.iter().size_hint());
 
+        ui.set_checked_for_coincidences(true);
+        
         println!("Líneas con consistencias: {:?}", lineas)
     });
 
     /*
         modify and save
      */
-    let ui_handle_modifynsave = ui.as_weak();
-    ui.on_request_modify_n_save(move || {
-        let ui = ui_handle_modifynsave.unwrap();
+    let ui_handle_modifycoincidence = ui.as_weak();
+    ui.on_request_modify_coincidence(move |index: i32| {
+        let ui = ui_handle_modifycoincidence.unwrap();
 
         let new_value = ui.get_selected_modify_value();
         let new_value = new_value.parse::<u32>().unwrap();
         let new_value = new_value.to_le_bytes();
-
-        let modification_index = ui.get_unique_coincidence_index();
-        let filename = ui.get_gamesaves().iter().next().unwrap().clone().to_string();
+        
+        let filename = ui.get_gamesave_list().iter().next().unwrap().clone().0.to_string();
         let file_ext = &[filename.split(".").last().unwrap()];
         let filename = filename.split(".").next().unwrap();
 
-        let file_to_clone_filepath = ui.get_gamesave_to_clone_filepath();
+        let file_to_clone_filepath = ui.get_gamesave_list().iter().next().unwrap().clone().1.to_string();
         let file_to_clone_folderpath = file_to_clone_filepath.split("\\").collect::<Vec<&str>>().deref().join("\\");
 
         let file_to_clone = inout::read_binary_file(&file_to_clone_filepath).unwrap();
-        let file_to_clone = inout::modify_file(file_to_clone, modification_index as u32, new_value.to_vec());
+        let file_to_clone = inout::modify_file(file_to_clone, index as u32, new_value.to_vec());
 
         let task = FileDialog::new().add_filter("original format", file_ext)
             .set_directory(file_to_clone_folderpath)
@@ -152,8 +160,39 @@ fn main() -> Result<(), Box<dyn Error>> {
             Some(val) => inout::store_file(file_to_clone, val.to_str().unwrap()).unwrap(),
             _ => println!("Catastrophic error...")
         }
-
     });
+    //
+    // let ui_handle_modifynsave = ui.as_weak();
+    // ui.on_request_modify_n_save(move |coincidence: i32| {
+    //     let ui = ui_handle_modifynsave.unwrap();
+    //
+    //     let new_value = ui.get_selected_modify_value();
+    //     let new_value = new_value.parse::<u32>().unwrap();
+    //     let new_value = new_value.to_le_bytes();
+    //
+    //     let modification_index = ui.get_selected_coincidence_index();
+    //     let filename = ui.get_gamesave_list().iter().next().unwrap().clone().0.to_string();
+    //     let file_ext = &[filename.split(".").last().unwrap()];
+    //     let filename = filename.split(".").next().unwrap();
+    //
+    //     let file_to_clone_filepath = ui.get_gamesave_to_clone_filepath();
+    //     let file_to_clone_folderpath = file_to_clone_filepath.split("\\").collect::<Vec<&str>>().deref().join("\\");
+    //
+    //     let file_to_clone = inout::read_binary_file(&file_to_clone_filepath).unwrap();
+    //     let file_to_clone = inout::modify_file(file_to_clone, modification_index as u32, new_value.to_vec());
+    //
+    //     let task = FileDialog::new().add_filter("original format", file_ext)
+    //         .set_directory(file_to_clone_folderpath)
+    //         .set_title("Save modified savegame")
+    //         .set_file_name(filename)
+    //         .save_file();
+    //
+    //     match task {
+    //         Some(val) => inout::store_file(file_to_clone, val.to_str().unwrap()).unwrap(),
+    //         _ => println!("Catastrophic error...")
+    //     }
+    //
+    // });
 
     ui.run()?;
 
--- src/ui_management.rs
+++ src/ui_management.rs
@@ -1,6 +1,6 @@
 pub fn add_gamesave() -> Option<(String, String)> {
     match rfd::FileDialog::new().set_directory(".").pick_file() {
-        Some(data) => Some((data.as_path().to_str().unwrap().to_string(), data.file_name()?.to_str()?.to_string())),
+        Some(data) => Some((data.file_name()?.to_str()?.to_string(), data.as_path().to_str().unwrap().to_string())),
         _ => None
     }
 }
